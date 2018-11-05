4f8c69f45cc5926ad841f8dd4c5ee4a9581f968d
PAWKAT\scraw <p.p.pawkat@gmail.com>
2018-11-05 11:50:22 +0200

    fix reply (paint)

--- src/js/_paint.js
+++ src/js/_paint.js
@@ -1,10 +1,10 @@
 export default class Paint {
-  constructor(){
+  constructor() {
     this.canvas = document.getElementById('paint');
     this.ctx = this.canvas.getContext('2d');
     this.ctx.strokeWidth = 0;
     this.settings = {
-      fill: "#000000",
+      fill: '#000000',
       lineWidth: 20
     };
     this.arcRadius = this.settings.lineWidth / 2;
@@ -14,7 +14,8 @@ export default class Paint {
     this.coords = [];
     this.init();
   }
-  init(){
+
+  init() {
     this._setSize();
     this._changeSize();
     this._detectMouse();
@@ -22,30 +23,35 @@ export default class Paint {
     this._draw();
     this._buttonsEvents();
   }
-  _setSize(){
+
+  _setSize() {
     this.canvas.width = window.innerWidth - 100;
     this.canvas.height = window.innerHeight;
   }
-  _changeSize(){
-    $(window).on('resize', ()=>{
-      this._setSize()
-    })
+
+  _changeSize() {
+    $(window).on('resize', () => {
+      this._setSize();
+    });
   }
-  _detectMouse(){
-    this.canvas.addEventListener('mousedown', ()=>{
+
+  _detectMouse() {
+    this.canvas.addEventListener('mousedown', () => {
       this.isMouseDown = true;
     });
-    this.canvas.addEventListener('mouseup', ()=>{
+    this.canvas.addEventListener('mouseup', () => {
       this.isMouseDown = false;
     });
   }
-  _mouseEvents(){
-    this.canvas.addEventListener('mouseup', ()=>{
+
+  _mouseEvents() {
+    this.canvas.addEventListener('mouseup', () => {
       this.ctx.beginPath();
       this.coords.push('mouseup');
     });
   }
-  _render(x, y, lineWidth, fill){
+
+  _render(x, y, lineWidth, fill) {
 
     let radius = lineWidth / 2;
 
@@ -57,14 +63,15 @@ export default class Paint {
     this.ctx.stroke();
 
     this.ctx.beginPath();
-    this.ctx.arc(x, y, radius, 0 , Math.PI * 2);
+    this.ctx.arc(x, y, radius, 0, Math.PI * 2);
     this.ctx.fill();
 
     this.ctx.beginPath();
     this.ctx.moveTo(x, y);
   }
-  _draw(){
-    this.canvas.addEventListener('mousemove', (e)=>{
+
+  _draw() {
+    this.canvas.addEventListener('mousemove', (e) => {
 
 
       if (this.isMouseDown) {
@@ -77,49 +84,52 @@ export default class Paint {
       }
     });
   }
-  _clear(){
-    this.ctx.rect(0,0,this.canvas.width,this.canvas.height);
+
+  _clear() {
+    this.ctx.rect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.fillStyle = '#ffffff';
     this.ctx.fill();
     this.ctx.beginPath();
   }
-  _save(){
+
+  _save() {
     let settings = JSON.stringify(this.settings);
     let coords = JSON.stringify(this.coords);
     localStorage.setItem('settings', settings);
     localStorage.setItem('coords', coords);
   }
-  _replay(){
+
+  _replay() {
     let settings = JSON.parse(localStorage.getItem('settings'));
     let coords = JSON.parse(localStorage.getItem('coords'));
-    console.log(coords);
-    let timer = setInterval(()=>{
+    this._clear();
+    let timer = setInterval(() => {
       if (!coords.length) {
         clearInterval(timer);
         this.ctx.beginPath();
         return;
       }
-      this._clear();
       let coord = coords.shift();
       console.log(coord[0], coord[1]);
-      this._render(coord[0], coord[1], settings.lineWidth, settings.fill)
+      this._render(coord[0], coord[1], settings.lineWidth, settings.fill);
 
-    }, 30)
+    }, 30);
   }
-  _buttonsEvents(){
+
+  _buttonsEvents() {
     let clear = document.getElementById('clear');
-    clear.addEventListener('click', ()=>{
+    clear.addEventListener('click', () => {
       this._clear();
     });
 
     let save = document.getElementById('save');
-    save.addEventListener('click', ()=>{
+    save.addEventListener('click', () => {
       this._save();
     });
 
     let replay = document.getElementById('replay');
-    replay.addEventListener('click', ()=>{
+    replay.addEventListener('click', () => {
       this._replay();
-    })
+    });
   }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,14 @@
-import cssTriangles from './_cssTriangles'
-import Triangles from './_canvasTriangles'
-import Ballz from './_ballz'
-import Paint from './_paint'
+import cssTriangles from './_cssTriangles';
+import Triangles from './_canvasTriangles';
+import Ballz from './_ballz';
+import Paint from './_paint';
 
 
-if ($('#triangles').length){
+if ($('#triangles').length) {
   let canvasTriangles = new Triangles;
   cssTriangles();
 }
-if ($('#ballz').length){
+if ($('#ballz').length) {
   let canvasBallz = new Ballz;
 }
 if ($('#paint').length) {
